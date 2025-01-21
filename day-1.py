7566696c7e9f0a0ab1e56c21417b068163c7e25b
chinthamanipavani <chintamanipavani2001@gmail.com>
2025-01-21 19:21:32 +0530

    py-1

---
+++ day-1.py
@@ -0,0 +1,47 @@
+
+print('ARITHMETIC OPERATIONS')
+print(4+3)
+print(0-3)
+print(5*12)
+print(6/2)
+print(9//3)
+print('')
+print('')
+print('variables')
+num1=12
+num2=3
+print(num1+num2)
+print(num1-num2)
+print(num1*num2)
+print(num1/num2)
+print(num1//num2)
+print('')
+print('')
+print('TYPE')
+num2=3.345
+num3='pavani'
+print(type(num1))
+print(type(num2))
+print(type(num3))
+print('')
+print('')
+print('STRINGS')
+str1='hello'
+print('good night')
+print("good night")
+print(str1)
+print('')
+print('')
+print('LENGTH')
+str2="    "
+print(len(str1))
+print(len(str2))
+print(str1[1])
+print(str2[2])
+print(str2[0])
+print(str1[-4])
+
+
+
+
+      
