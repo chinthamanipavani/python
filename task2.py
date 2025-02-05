95e6cbd3e28a905230228945f317f890b899fda8
pavani <162785057+chinthamanipavani@users.noreply.github.com>
2025-02-05 12:16:03 +0530

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,16 +1,23 @@
+ 
+#  1
 n1=100
 while n1<101:
     print(n1)
     n1+=1
 
+# for i in range(1,101):
+#  print(i)
 
 
+# 2
 num2=100
 while num2>=1:
     print(num2)
     num2-=1
 
 
+
+# 3
 
 n=1234
 r=0
