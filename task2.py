c4d1ee39e9fb95cbda2ae5e4968b454deefe9955
pavani <162785057+chinthamanipavani@users.noreply.github.com>
2025-02-05 12:12:59 +0530

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,21 @@
+n1=100
+while n1<101:
+    print(n1)
+    n1+=1
+
+
+
+num2=100
+while num2>=1:
+    print(num2)
+    num2-=1
+
+
+
+n=1234
+r=0
+while (n>0):
+    r=r*10
+    r=r+n%10
+    n=n//10
+print(r)
