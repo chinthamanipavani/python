ada36e5118b98a1937d9889f31d7c3f5f4727585
pavani <162785057+chinthamanipavani@users.noreply.github.com>
2025-02-12 15:32:13 +0530

    Add files via upload

---
+++ function-2.py
@@ -0,0 +1,5 @@
+def task(**details):
+    for i,j in details.items():
+        print(i,j)
+task(user_name ='  pavani',password='  abc', email='  abc@gmail.com' ) 
+task(user_FirstName='  bhanu',user_secondName='  sree', email='  bhanu@gmail.com') 
